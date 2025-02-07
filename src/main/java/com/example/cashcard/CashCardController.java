4f19934a32c32363d7bc788d1a6fa9d43f3f7eca
Pawel Czajkowski <pawelek.czajkowski@gmail.com>
2025-02-07 18:37:39 +0100

    Add in-memory H2 database

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -1,5 +1,7 @@
 package com.example.cashcard;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,12 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+    private final CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
 
     @GetMapping("/{cardId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long cardId) {
-        if (cardId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
+        Optional<CashCard> optionalCashCard = cashCardRepository.findById(cardId);
+        if (optionalCashCard.isPresent()) {
+            return ResponseEntity.ok(optionalCashCard.get());
         } else {
             return ResponseEntity.notFound().build();
         }
